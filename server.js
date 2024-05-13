1ce4d685e730ec381bcaf88b91de6c008b810378
Christian <111076892+i-christian@users.noreply.github.com>
2024-05-13 14:27:43 +0200

    updated server.js by add express.json middleware

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/', routes);
 
