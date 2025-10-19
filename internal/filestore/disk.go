198b374b1c993b61aa548da87eb781109bc99f74
christian <111076892+i-christian@users.noreply.github.com>
2025-10-19 16:14:25 +0200

    feat: implement disk file management

---
+++ internal/filestore/disk.go
@@ -0,0 +1,74 @@
+package filestore
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// DiskStorage is an implementation of the FileStorage interface that stores files on the local disk.
+type DiskStorage struct {
+	root *os.Root
+}
+
+// NewDiskStorage is a constructor for DiskStorage.
+// It initializes a new DiskStorage instance, creating an os.Root rooted
+// at the specified `baseDir`. All subsequent file operations performed
+// through this DiskStorage instance will be confined within `baseDir`.
+func NewDiskStorage(baseDir string) (*DiskStorage, error) {
+	root, err := os.OpenRoot(baseDir)
+	if err != nil {
+		return nil, err
+	}
+	return &DiskStorage{root: root}, nil
+}
+
+// It ensures that the necessary parent directories for the `path` exist in root directory before creating file.
+func (s *DiskStorage) ensureParent(inputPath string) error {
+	rootPath := s.GetRootPath()
+	path := filepath.Join(rootPath, inputPath)
+
+	if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Save writes the content of the provided io.Reader (`file`) to the
+// specified `path` within the DiskStorage's root directory
+func (s *DiskStorage) Save(file io.Reader, inputPath string) error {
+	err := s.ensureParent(inputPath)
+	if err != nil {
+		return err
+	}
+
+	out, err := s.root.Create(inputPath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Copy the content from the input reader to the newly created file.
+	_, err = io.Copy(out, file)
+	return err
+}
+
+// Get retrieves the content of the file at the specified `path` from the DiskStorage's root directory.
+func (s *DiskStorage) Get(path string) (io.Reader, error) {
+	return s.root.Open(path)
+}
+
+// GetPath retrieves the root path to files directory
+func (s *DiskStorage) GetRootPath() string {
+	return s.root.Name()
+}
+
+// Delete removes the file at the specified `path` from the DiskStorage's root directory.
+func (s *DiskStorage) Delete(inputPath string) error {
+	rootPath := s.GetRootPath()
+
+	deletePath := filepath.Join(rootPath, inputPath)
+
+	return os.RemoveAll(deletePath)
+}
---
+++ internal/filestore/file_storage.go
@@ -0,0 +1,21 @@
+package filestore
+
+import "io"
+
+// FileStorage defines the interface for file storage operations.
+// Implementations of this interface are responsible for saving, retrieving,
+// and deleting files from a persistent storage medium i.e disk or amazon S3 buckets.
+type FileStorage interface {
+	// Save writes the content of the provided io.Reader to the specified path.
+	Save(file io.Reader, path string) error
+
+	// Get retrieves the content of the file at the specified path.
+	// It returns an io.Reader from which the file's content can be read.
+	Get(path string) (io.Reader, error)
+
+	// GetRootPath returns the root path of files directory
+	GetRootPath() string
+
+	// Delete removes the file at the specified path from storage.
+	Delete(path string) error
+}
