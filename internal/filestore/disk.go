5d3d25808b6d3d949e140c93048fb57a9d5d745e
christian <111076892+i-christian@users.noreply.github.com>
2026-02-28 19:07:11 +0200

    refactor: allow bulk deletion of files

--- internal/filestore/disk.go
+++ internal/filestore/disk.go
@@ -1,31 +1,36 @@
 package filestore
 
 import (
+	"context"
 	"io"
+	"log/slog"
 	"os"
 	"path/filepath"
+
+	"github.com/i-christian/fileShare/internal/utils"
 )
 
 // DiskStorage is an implementation of the FileStorage interface that stores files on the local disk.
 type DiskStorage struct {
-	root *os.Root
+	root   *os.Root
+	logger *slog.Logger
 }
 
 // NewDiskStorage is a constructor for DiskStorage.
 // It initializes a new DiskStorage instance, creating an os.Root rooted
 // at the specified `baseDir`. All subsequent file operations performed
 // through this DiskStorage instance will be confined within `baseDir`.
-func NewDiskStorage(baseDir string) (*DiskStorage, error) {
+func NewDiskStorage(baseDir string, logger *slog.Logger) (*DiskStorage, error) {
 	root, err := os.OpenRoot(baseDir)
 	if err != nil {
 		return nil, err
 	}
-	return &DiskStorage{root: root}, nil
+	return &DiskStorage{root: root, logger: logger}, nil
 }
 
 // It ensures that the necessary parent directories for the `path` exist in root directory before creating file.
 func (s *DiskStorage) ensureParent(inputPath string) error {
-	rootPath := s.GetRootPath()
+	rootPath := s.getRootPath()
 	path := filepath.Join(rootPath, inputPath)
 
 	if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
@@ -37,7 +42,7 @@ func (s *DiskStorage) ensureParent(inputPath string) error {
 
 // Save writes the content of the provided io.Reader (`file`) to the
 // specified `path` within the DiskStorage's root directory
-func (s *DiskStorage) Save(file io.Reader, inputPath string) (size int64, err error) {
+func (s *DiskStorage) Save(ctx context.Context, file io.Reader, inputPath string) (size int64, err error) {
 	err = s.ensureParent(inputPath)
 	if err != nil {
 		return 0, err
@@ -49,26 +54,38 @@ func (s *DiskStorage) Save(file io.Reader, inputPath string) (size int64, err er
 	}
 	defer out.Close()
 
-	// Copy the content from the input reader to the newly created file.
 	size, err = io.Copy(out, file)
 	return size, err
 }
 
 // Get retrieves the content of the file at the specified `path` from the DiskStorage's root directory.
-func (s *DiskStorage) Get(path string) (io.ReadCloser, error) {
+func (s *DiskStorage) Get(ctx context.Context, path string) (io.ReadCloser, error) {
 	return s.root.Open(path)
 }
 
-// GetRootPath retrieves the root path to files directory
-func (s *DiskStorage) GetRootPath() string {
+// getRootPath retrieves the root path to files directory
+func (s *DiskStorage) getRootPath() string {
 	return s.root.Name()
 }
 
 // Delete removes the file at the specified `path` from the DiskStorage's root directory.
-func (s *DiskStorage) Delete(inputPath string) error {
-	rootPath := s.GetRootPath()
+func (s *DiskStorage) Delete(ctx context.Context, paths []string) (successCount, failureCount int, err error) {
+	if len(paths) == 0 {
+		return 0, 0, utils.ErrFilesNotFound
+	}
 
-	deletePath := filepath.Join(rootPath, inputPath)
+	for idx := range paths {
+		err := s.root.RemoveAll(paths[idx])
+		if err != nil {
+			failureCount += 1
 
-	return os.RemoveAll(deletePath)
+			utils.WriteServerError(s.logger, "failed to delete disk file", err)
+			continue
+
+		}
+
+		successCount += 1
+	}
+
+	return successCount, failureCount, nil
 }
